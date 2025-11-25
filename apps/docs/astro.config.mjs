12cf31021b89c9ec6fb279d6427dba84249219e1
drive <driveasano@gmail.com>
2025-11-25 14:38:23 +0900

    feat: add new Astro Starlight documentation site with initial guides and localized content.

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,50 +1,61 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator'
+import starlightImageZoom from 'starlight-image-zoom'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mosunset.github.io/youtube_watch_thumbnails',
-	integrations: [
-		starlight({
-			logo: {
-				src: '/src/assets/icon.png',
-				alt: 'YouTube Watch Thumbnails',
-				replacesTitle: false,
-			},
-			title: 'YouTube Watch Thumbnails',
-			defaultLocale: 'root',
-			locales: {
-				root: {
-					label: 'English',
-					lang: 'en',
-				},
-				ja: {
-					label: '日本語',
-					lang: 'ja',
-				},
-			},
-			social: [
-				{
-					label: 'GitHub',
-					icon: 'github',
-					href: 'https://github.com/mosunset/youtube_watch_thumbnails',
-				},
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					autogenerate: { directory: 'guides' },
-				},
-				{
-					label: 'Resources',
-					items: [
-						{ label: 'FAQ', slug: 'faq' },
-						{ label: 'Privacy Policy', slug: 'privacy' },
-						{ label: 'Changelog', slug: 'changelog' },
-					],
-				},
-			],
-		}),
-	],
+    site: 'https://mosunset.github.io',
+	base: '/youtube_watch_thumbnails',
+    integrations: [starlight({
+        plugins: [starlightLinksValidator(),starlightImageZoom()],
+        favicon: '/favicon.ico',
+        logo: {
+            src: '/src/assets/icon.png',
+            alt: 'YouTube Watch Thumbnails',
+            replacesTitle: false,
+        },
+        title: 'YouTube Watch Thumbnails',
+        editLink: {
+            baseUrl: 'https://github.com/mosunset/youtube_watch_thumbnails/blob/main/apps/docs/',
+        },
+        defaultLocale: 'root',
+        locales: {
+            root: {
+                label: 'English',
+                lang: 'en',
+            },
+            ja: {
+                label: '日本語',
+                lang: 'ja',
+            },
+        },
+        social: [
+            {
+                label: 'GitHub',
+                icon: 'github',
+                href: 'https://github.com/mosunset/youtube_watch_thumbnails',
+            },
+        ],
+        sidebar: [
+            {
+                label: 'Guides',
+                autogenerate: { directory: 'guides' },
+            },
+            {
+                label: 'Resources',
+                autogenerate: { directory: 'resources' },
+                // items: [
+                // 	{ label: 'FAQ', slug: 'faq' },
+                // 	{ label: 'Privacy Policy', slug: 'privacy' },
+                // 	{ label: 'Changelog', slug: 'changelog' },
+                // ],
+            }, {
+                label: 'Other',
+                autogenerate: { directory: 'other' },
+            }
+        ],
+    }), sitemap()],
 });
